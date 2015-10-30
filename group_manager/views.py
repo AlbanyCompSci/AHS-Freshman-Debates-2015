8a102f7c52456bb3c1f36d40fe4e34ce99fecd1b
ilay <ilay.raz@gmail.com>
2015-10-29 20:45:09 -0700

    Adding sample login_required view

--- group_manager/views.py
+++ group_manager/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+@login_required
+def index(request):
+    HttpResponse("It's working!")
