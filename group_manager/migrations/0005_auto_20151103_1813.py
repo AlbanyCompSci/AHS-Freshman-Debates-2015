7dafd0f82e8a29ec694b3dbc96f6aeeb1fe9f01b
ilay <ilay.raz@gmail.com>
2015-11-03 10:14:44 -0800

    Studend.group can now be blank

---
+++ group_manager/migrations/0005_auto_20151103_1813.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('group_manager', '0004_auto_20151103_0319'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='student',
+            name='group',
+            field=models.ForeignKey(null=True, to='group_manager.Student_Group'),
+        ),
+    ]
