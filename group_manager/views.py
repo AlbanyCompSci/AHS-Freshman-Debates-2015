cc835448ffab5e44744c85086442377a280fbd0c
ilay <ilay.raz@gmail.com>
2015-11-03 18:37:41 -0800

    Adding groups:index as genericView

--- group_manager/views.py
+++ group_manager/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
-from django.http import HttpResponse
+from django.views import generic
+from .models import *
 
 # Create your views here.
 
 
-@login_required
+"""@login_required
 def index(request):
-    return HttpResponse("It's working!")
+    my_groups = Student_Group.objects.filter(teacher=request.user)
+    return render(request, 'group_manager/index.html', {'groups': my_groups})"""
+
+@login_required
+class IndexView (generic.ListView):
+    template_name = 'group_manager/index.html'
+    context_object_name = 'my_groups'
+
+    def get_queryset (self):
+        """Returns all groups belonging to user"""
+        return Student_Group.objects.filter(teacher=self.request.user)
