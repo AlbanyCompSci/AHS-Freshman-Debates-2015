3458f145cf995a9d23e066c924030ce8a16d804c
Ilay Raz <ilay.raz@gmail.com>
2015-11-30 19:21:42 -0800

    Adding all models to admin site

--- group_manager/admin.py
+++ group_manager/admin.py
@@ -4,18 +4,8 @@ from . import models
 # Register your models here.
 
 
-class StudentInline(admin.TabularInline):
-    """Stacked Inline View for Student"""
-    model = models.Student
-    min_num = 2
-    max_num = 20
-    extra = 3
-
-
-class GroupAdmin (admin.ModelAdmin):
-    fields = ['teacher']
-    inlines = [StudentInline]
-
-admin.site.register(models.Student_Group, GroupAdmin)
+admin.site.register(models.Student_Group)
 admin.site.register(models.Student_Class)
+admin.site.register(models.Student)
+admin.site.register(models.Judge)
 admin.site.register(models.Debate_Group)
