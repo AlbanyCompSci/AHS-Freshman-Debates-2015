3f367d65a59338666f1797322ad6f3101c5f680c
ilay <ilay.raz@gmail.com>
2015-12-01 10:49:03 -0800

    Adding debate delete

--- group_manager/urls.py
+++ group_manager/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.views import generic
+from django.core.urlresolvers import reverse_lazy
 from . import views
 from . import models
 
@@ -8,19 +9,29 @@ urlpatterns = [
     url(r'^group/(?P<pk>[0-9]+)/$',
         generic.DetailView.as_view(model=models.Student_Group),
         name='group_detail'),
-    url(r'^student/(?P<pk>[0-9]+)/$',
-        generic.DetailView.as_view(model=models.Student),
-        name='student_detail'),
+    url(r'^group/(?P<pk>[0-9]+)/update/$', generic.UpdateView.as_view(
+        model=models.Student_Group,
+        fields='__all__'), name='group_update'),
+    url(r'^group/(?P<pk>[0-9]+)/delete/$', generic.DeleteView.as_view(
+        model=models.Student_Group,
+        success_url=reverse_lazy('groups:index')), name='group_delete'),
     url(r'^create/(?P<teacher>[0-9]+)/group/$',
         views.StudentGroupPeriodSelect, name='student_group_period_select'),
     url(r'^create/(?P<teacher>[0-9]+)/(?P<periods>[1-7]+)/group/$',
         views.StudentGroupCreate.as_view(), name='student_group_form'),
+    url(r'^debate/$', generic.ListView.as_view(model=models.Debate_Group),
+        name='debate_index'),
     url(r'^debate/(?P<pk>[0-9]+)/$',
         generic.DetailView.as_view(model=models.Debate_Group),
         name='debate_detail'),
+    url(r'^debate/(?P<pk>[0-9]+)/delete/$', generic.DeleteView.as_view(
+            model=models.Debate_Group,
+            success_url=reverse_lazy('groups:debate_index')),
+        name='debate_delete'),
     url(r'^create/debate/$', generic.CreateView.as_view(
             model=models.Debate_Group,
             fields='__all__'), name='debate_create'),
-    url(r'^debate/$', generic.ListView.as_view(model=models.Debate_Group),
-        name='debate_index')
+    url(r'^student/(?P<pk>[0-9]+)/$',
+        generic.DetailView.as_view(model=models.Student,),
+        name='student_detail'),
 ]
