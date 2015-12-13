16c93723fab2c068dabe46d1776136662a147bb1
Ilay Raz <ilay.raz@gmail.com>
2015-12-13 00:50:30 -0800

    Adding GroupMixin to admin, JudgeGroupAdmin, and its form

--- group_manager/admin.py
+++ group_manager/admin.py
@@ -4,30 +4,45 @@ from . import models
 # Register your models here.
 
 
-@admin.register(models.Student_Group)
-class StudentGroupAdmin (admin.ModelAdmin):
-    form = forms.StudentGroupForm
+class GroupMixin (object):
+    def __init__(self, name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fieldName = name
 
     def get_form(self, request, obj=None, **kwargs):
-        """ Returns form with current user passed """
+        """ Returns form with current user passed in """
         form = super().get_form(request, obj, **kwargs)
         form.current_user = request.user
         return form
 
+    def save_related(self, request, form, formsets, change):
+        super()
+        form.fields[self.fieldName].initial.update(group=None)
+        form.cleaned_data[self.fieldName].update(group=form.instance)
+
+
+@admin.register(models.Student_Group)
+class StudentGroupAdmin (GroupMixin, admin.ModelAdmin):
+    form = forms.StudentGroupForm
+
+    def __init__(self, *args, **kwargs):
+        super().__init__('students', *args, **kwargs)
+
     def save_form(self, request, form, change):
-        """
-            Resets all inital students (in case some were removed)
-            Sets teacher to current user
-        """
-        form_uncommited = form.save(commit=False)
+        """ Set teacher to current user """
+        form_uncommited = super().save_form(request, form, change)
         form_uncommited.teacher = request.user
-        form.fields['students'].initial.update(group=None)
         return form_uncommited
 
-    def save_related(self, request, form, formsets, change):
-        """ Sets selected students' groups to this group """
-        super()
-        form.cleaned_data['students'].update(group=form.instance)
+
+@admin.register(models.Judge_Group)
+class JudgeGroupAdmin (GroupMixin, admin.ModelAdmin):
+    form = forms.JudgeGroupForm
+
+    def __init__(self, *args, **kwargs):
+        super().__init__('judges', *args, **kwargs)
+
+
 
 admin.site.register(models.Student_Class)
 admin.site.register(models.Student)
@@ -36,4 +51,3 @@ admin.site.register(models.Debate_Group)
 admin.site.register(models.Location)
 admin.site.register(models.Schedule)
 admin.site.register(models.Debate)
-admin.site.register(models.Judge_Group)
--- group_manager/forms.py
+++ group_manager/forms.py
@@ -19,15 +19,6 @@ class StudentGroupForm (forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # For creation
-        self.fields['students'] = forms.ModelMultipleChoiceField(
-                queryset=models.Student.objects.filter(
-                        group__isnull=True,
-                        english_class__teacher=self.current_user),
-                widget=FilteredSelectMultiple(
-                        verbose_name="Students",
-                        is_stacked=False))
-
         # For change. Includes already selected students
         if self.instance:
             self.fields['students'] = forms.ModelMultipleChoiceField(
@@ -39,3 +30,40 @@ class StudentGroupForm (forms.ModelForm):
                             verbose_name="Students",
                             is_stacked=False))
             self.fields['students'].initial = self.instance.student_set.all()
+        else:
+            self.fields['students'] = forms.ModelMultipleChoiceField(
+                queryset=models.Student.objects.filter(
+                        group__isnull=True,
+                        english_class__teacher=self.current_user),
+                widget=FilteredSelectMultiple(
+                        verbose_name="Students",
+                        is_stacked=False))
+
+
+class JudgeGroupForm (forms.ModelForm):
+    class Meta:
+        model = models.Judge_Group
+        fields = '__all__'
+
+    judges = forms.ModelMultipleChoiceField(
+                                        queryset=models.Judge.objects.all())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # For change, includes already selected judges
+        if self.instance:
+            self.fields['judges'] = forms.ModelMultipleChoiceField(
+                    queryset=models.Judge.objects.filter(
+                            Q(group__isnull=True) | Q(group=self.instance)),
+                    widget=FilteredSelectMultiple(
+                            verbose_name="Judges",
+                            is_stacked=False))
+            self.fields['judges'].initial = self.instance.judge_set.all()
+        else:
+            # For creation
+            self.fields['students'] = forms.ModelMultipleChoiceField(
+                    queryset=models.Judge.objects.filter(group__isnull=True),
+                    widget=FilteredSelectMultiple(
+                            verbose_name="Judges",
+                            is_stacked=False))
