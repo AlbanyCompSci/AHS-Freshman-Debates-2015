db83b9129d86ba6a33da641d38d0eb582df80c07
Ilay Raz <ilay.raz@gmail.com>
2016-02-09 22:21:56 -0800

    Removing development tools from production

--- AHS_Freshman_Debates/settings.py
+++ AHS_Freshman_Debates/settings.py
@@ -42,7 +42,6 @@ DEFAULT_APPS = (
 )
 
 THIRD_PARTY_APPS = (
-    'debug_toolbar',
     'grappelli'
 )
 
@@ -121,24 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# DEBUG TOOLBAR
-# Modify SHOW_TOOLBAR_CALLBACK at production
-
-
-def show_toolbar_overwrite(request):
-    if request.is_ajax():
-        return False
-
-    return bool(DEBUG)
-
-DEBUG_TOOLBAR_CONFIG = {
-    'DEBUG_TOOLBAR_PATCH_SETTINGS': False,
-    'SHOW_TOOLBAR_CALLBACK': show_toolbar_overwrite
-}
-
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-SHOW_TOOLBAR_CALLBACK = show_toolbar_overwrite
 
 # GRAPPELLI
 
