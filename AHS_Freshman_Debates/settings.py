ca2029b5cc3f475ee6f1a70920c4cc8d8b1dcea7
Ilay Raz <ilay.raz@gmail.com>
2016-02-09 22:22:25 -0800

    Hiding secret key

--- AHS_Freshman_Debates/settings.py
+++ AHS_Freshman_Debates/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pf^j$#@6*h0z)a-=sx8=%%pqnsf*7c$!_dh9c*_wdmz@#mvhem'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
