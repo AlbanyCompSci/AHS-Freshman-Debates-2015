99daa1d0528750df19e9928169b80b5664504ff6
Ilay Raz <ilay.raz@gmail.com>
2016-03-09 21:07:44 -0800

    fixing settings.py

--- AHS_Freshman_Debates/settings.py
+++ AHS_Freshman_Debates/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ahs-freshman-debates.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
